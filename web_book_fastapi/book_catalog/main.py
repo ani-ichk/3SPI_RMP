9ff04db554b770b33e245ddc97388cd4d7878c47
ani-ichk <akumic8400@email.com>
2026-06-14 22:32:12 +0900

    67

--- web_book_fastapi/book_catalog/main.py
+++ web_book_fastapi/book_catalog/main.py
@@ -1,15 +1,19 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
 from applifespan import lifespan
-from rest.main_views import router as api_router
+from rest.main_views import router as rest_router
+from web_book_fastapi.book_catalog.api.api_v1.books.views.list_views import router as list_router
+from web_book_fastapi.book_catalog.api.api_v1.books.views.details_views import router as details_router
 
 
-app = FastAPI(
-    title='Books',
-    lifespan=lifespan,
-)
+api_router = APIRouter(prefix="/api/v1")
+api_router.include_router(list_router)
+api_router.include_router(details_router)
 
+app = FastAPI(title='Books', lifespan=lifespan)
+
+app.include_router(rest_router)
 app.include_router(api_router)
 
 
